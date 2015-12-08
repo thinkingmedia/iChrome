31d780029133bc9e529396de20279abf2be64c4a
thinkingmedia <mathew@thinkingmedia.ca>
2015-12-08 14:28:27 -0500

    updated

--- app/widgets/whatsmyip/js/model.js
+++ app/widgets/whatsmyip/js/model.js
@@ -1,71 +1,72 @@
 define(["lodash", "jquery", "widgets/model"], function (_, $, WidgetModel) {
 
-    var matchIPV4 = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gi;
+	var matchIPV4 = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gi;
 
-    return WidgetModel.extend({
+	return WidgetModel.extend({
 
-        /**
-         * Once per hour.
-         */
-        refreshInterval: 3600000,
+		/**
+		 * Once per hour.
+		 */
+		refreshInterval: 3600000,
 
-        defaults: {
-            config: {
-                size: "variable",
-                provider: "http://checkip.dyndns.org/",
-                title: "Whats My IP"
-            }
-        },
-
-        /**
-         * Initialize
-         */
-        initialize: function () {
-            WidgetModel.prototype.initialize.call(this);
-
-            this.saveData({
-                title: this.config.title || this.defaults.config.title
-            });
-
-			if(!this.data.external) {
-				this.data.external = '0.0.0.0';
-				this.data.internal = '0.0.0.0';
+		defaults: {
+			config: {
+				size: "variable",
+				provider: "http://checkip.dyndns.org/",
+				title: "Whats My IP"
 			}
-        },
+		},
 
-		// @todo - this should be done as a service encase there are multiple widgets
-        refresh: function () {
-            var self = this;
+		/**
+		 * Initialize
+		 */
+		initialize: function () {
+			WidgetModel.prototype.initialize.call(this);
 
-            if(!this.config.provider) {
-                return;
-            }
+			this.saveData({
+				title: this.config.title || this.defaults.config.title
+			});
+		},
 
-            this.trigger('refresh:start', this, { widgetChange: true });
+		isCached: function () {
+			return !!this.data.values;
+		},
 
-            $.ajax({
-                url: this.config.provider,
-                method: "GET",
-                cache: false,
-                dataType: "text",
-                success: function (data, textStatus, jqHXR) {
-                    var matches = (data || "").match(matchIPV4);
-                    if (jqHXR.status != 200 || !_.isArray(matches) || matches.length == 0) {
-                        self.trigger('refresh:failed', self, { widgetChange: true });
-                        return;
-                    }
+		refresh: function () {
+			var self = this;
 
-                    self.data.external = matches[0];
-                    self.saveData();
+			if (!this.config.provider) {
+				return;
+			}
 
-                    console.log(matches);
+			this.trigger('refresh:start', this, {widgetChange: true});
 
-                    self.trigger('refresh:finish', self, { widgetChange: true });
-                },
-                error: function () {
-                    self.trigger('refresh:failed', self, { widgetChange: true });
-                }
-            });
-        }
-    });
+			$.ajax({
+				url: this.config.provider,
+				method: "GET",
+				cache: false,
+				dataType: "text",
+				success: function (data, textStatus, jqHXR) {
+					var matches = (data || "").match(matchIPV4);
+					if (jqHXR.status != 200 || !_.isArray(matches) || matches.length == 0) {
+						self.trigger('refresh:failed', self, {widgetChange: true});
+						return;
+					}
+
+					self.saveData({
+						values: {
+							external: matches[0],
+							internal: '0.0.0.0',
+							next: new Date()
+						}
+					});
+
+					self.trigger('refresh:finish', self, {widgetChange: true});
+				},
+				error: function () {
+					self.trigger('refresh:failed', self, {widgetChange: true});
+				}
+			});
+		}
+	});
 });
